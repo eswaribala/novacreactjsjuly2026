58bcf17eb05905b56139d0de5f0d13e406b2e439
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-07-28 12:40:54 +0530

    updated

---
+++ kyc-app/src/components/atoms/Button/Button.jsx
@@ -0,0 +1,10 @@
+
+function Button({ label, onClick, type = 'button', disabled = false }) {
+  return (
+    <button type={type} onClick={onClick} disabled={disabled}>
+      {label}
+    </button>
+  );
+}
+
+export default Button;
