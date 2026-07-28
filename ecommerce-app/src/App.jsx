6c1c302c8bddca7572346e01ffb6efb8e8114b2b
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-07-28 10:21:57 +0530

    updated

--- ecommerce-app/src/App.jsx
+++ ecommerce-app/src/App.jsx
@@ -1,12 +1,27 @@
 
+import Banner from './components/atoms/Banner/Banner.jsx';
 import Logo from './components/atoms/Logo/Logo.jsx';
 
+
+import { useState } from 'react';
+
+
 function App() {
-
-
+  const [activeMenu, setActiveMenu] = useState("Dashboard");
   return (
     <>
-      <Logo/>
+    <div className="ml-10 mt-2 mr-10">
+    <header className="flex h-40 w-full bg-blue-100">
+      <div className="flex w-80 shrink-0 items-center justify-center px-8">
+        <Logo />
+      </div>
+
+      <div className="min-w-0 flex-1 overflow-hidden">
+        <Banner />
+      </div>
+    </header>
+    </div>
+    
     </>
   )
 }
--- ecommerce-app/src/components/atoms/Logo/Logo.jsx
+++ ecommerce-app/src/components/atoms/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import shoplogo from './../../../assets/shoplogo.png';
 
 const Logo = () => (
   
-    <img src={shoplogo} alt="Logo" className='w-10 h-10 rounded-full'/>
+    <img src={shoplogo} alt="Logo" className='w-full h-auto rounded-full'/>
  
 );
 
