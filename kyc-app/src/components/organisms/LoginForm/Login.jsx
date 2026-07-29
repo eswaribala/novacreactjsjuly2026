f2d8a179a45a0ea27f7071862b6f8a81e8f22ce9
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-07-29 13:01:20 +0530

    updated

--- kyc-app/src/components/organisms/LoginForm/Login.jsx
+++ kyc-app/src/components/organisms/LoginForm/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import FormField from '../../molecules/FormField/FormField';
 import PrimeButton from '../../atoms/PrimeButton/PrimeButton';
+import Input from '../../atoms/Input/Input';
+import Label from '../../atoms/Label/Label';
 
 function Login({isSubmitting,onLogin,onRegister}){
 
@@ -18,8 +20,15 @@ function Login({isSubmitting,onLogin,onRegister}){
         setValues({ ...values,[name]:value});
     }
 
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log('Form submitted with values:', values);
+        onLogin(values,rememberMe);
+    }
+
     return (
-        <form>
+        <>
+        <form onSubmit={handleSubmit}>
            <FormField 
            id="name" 
            name="name" 
@@ -76,6 +85,39 @@ function Login({isSubmitting,onLogin,onRegister}){
 
 
         </form>
+
+        <div className="mt-4">
+            <Input 
+            type="checkbox" 
+            id="rememberMe" 
+            checked={rememberMe} 
+            onChange={(e) => setRememberMe(e.target.checked)} />
+            <Label htmlFor="rememberMe">Remember Me</Label>
+        </div>
+        <div className="flex justify-center">
+            <PrimeButton 
+            id="forgotPassword" 
+            label="Forgot Password" 
+            className="h-14
+            w-72
+            rounded-xl
+            bg-red-600
+            text-lg
+            font-semibold
+            text-white
+            shadow-md
+            transition-all
+            duration-300
+            hover:bg-red-700
+            hover:shadow-lg
+            focus:outline-none
+            focus:ring-4
+            focus:ring-red-200">
+                Forgot Password
+            </PrimeButton>
+        </div>
+
+        </>
     );
     
 
