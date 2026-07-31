5cf26d2f19f1bfb27718578c46af9d60bfe1a9cb
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-07-31 15:50:33 +0530

    updated

--- ecommerce-app/src/components/molecules/SideBarMenu/SideBarMenu.jsx
+++ ecommerce-app/src/components/molecules/SideBarMenu/SideBarMenu.jsx
@@ -1,10 +1,24 @@
 //create SideBarMenu component from MenuItem
+
 import MenuItem from '../atoms/MenuItem/MenuItem';
-function SideBarMenu() {
+function SideBarMenu({menu,isOpen,onToggle}) {
     return(
         <div className="sidebar-menu">
-            <MenuItem label="Home" icon="🏠" path="/" />
+            <MenuItem label={menu.label} icon={menu.icon} path={menu.path}
+            hasChildren={menu.hasChildren} onClick={() => onToggle(menu.id)} 
+            isOpen={isOpen} />
+            {isOpen && menu.children && (
+                <div className="sidebar-submenu">
+                    {menu.children.map((child) => (
+                        <MenuItem key={child.id} label={child.label} icon={child.icon} path={child.path}
+                        hasChildren={child.hasChildren} onClick={() => onToggle(child.id)} 
+                        isOpen={isOpen} />
+                    ))}
+                </div>
+            )}
         </div>
     )
 
 }
+
+export default SideBarMenu;
