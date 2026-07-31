8ccb8046eb486c0037615e9aa44eccacbfe8c62d
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-07-31 11:17:17 +0530

    updated

---
+++ kyc-auth-api/controllers/registrationController.js
@@ -0,0 +1,28 @@
+const User=require('../models/userModel');
+
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        if (!name || !email || !password) {
+            return res.status(400).json(
+                { message: 'All fields are required' });
+        }
+
+        const existingUser = await User.findOne({ name });
+        if (existingUser) {
+            return res.status(400).json(
+                { message: 'User already exists' });
+        }
+
+        const newUser = new User({ name, email, password });
+        await newUser.save();
+        res.status(201).json(
+            { message: 'User registered successfully' });
+    } catch (error) {
+        res.status(500).json(
+            { message: 'Server error' });
+    }
+};
+
+module.exports = { registerUser };
