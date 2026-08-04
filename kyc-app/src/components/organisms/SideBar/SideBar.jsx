db493a58086d9b11384e175ed6f1290a2b4bb565
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-04 10:58:31 +0530

    updated

---
+++ kyc-app/src/components/organisms/SideBar/SideBar.jsx
@@ -0,0 +1,34 @@
+import {menuData} from '../../../data/menuData';
+import SideBarMenu from '../../molecules/SideBarMenu/SideBarMenu';
+import {useState} from 'react';
+import MenuItem from '../../atoms/MenuItem/MenuItem';
+
+function SideBar() {
+     const [openMenu, setOpenMenu] = useState('orders');
+    const handleToggle = (menuId) => { setOpenMenu((current) => (current === menuId ? null : menuId)); };
+
+    return (
+        <aside className="sidebar">
+      <nav
+        className="sidebar-navigation font-bold text-xl"
+        aria-label="Main navigation"
+      >
+           
+                {menuData.map((menuItem) => {
+                    return menuItem.children ? (
+                        <SideBarMenu key={menuItem.id} 
+                        isOpen={openMenu === menuItem.id} 
+                        onToggle={handleToggle} menu={menuItem}/>
+                    ) : (
+                        <MenuItem key={menuItem.id}{...menuItem}
+                            
+                        />
+                    );
+                })}
+          
+        </nav>
+        </aside>
+    );
+}
+
+export default SideBar;
