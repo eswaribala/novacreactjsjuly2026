c2a1c536b9c145ccd6cc4810b33fa1aa2518d8c2
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-04 10:18:33 +0530

    updated

--- kyc-app/src/data/menuData.js
+++ kyc-app/src/data/menuData.js
@@ -2,36 +2,78 @@ import {Asterisk} from 'lucide-react'
 import {ArrowLeftRight,Summary,UserPen,MessageCircleX} from 'lucide-react'
 export const menuData = [
     {
-        id: 'Masters',
+        id: 'masters',
         label: 'Masters',
         icon: Asterisk,
         children: [
+            {
+                id: 'change-password',
+                label: 'Change Password',
+                path: 'masters/change-password'
+            }
 
         ]
     },
     {
-        id: 'Transactions',
+        id: 'transactions',
         label: 'Transactions',
         icon: ArrowLeftRight,
         children: [
+            {
+                id:'beneficiary-pan',
+                label:'Beneficiary PAN',
+                path:'transactions/beneficiary-pan'
+            },
+            {
+                id:'policy-service',
+                label:'Policy Service',
+                children:[{
+                    id:'payouts',
+                    label:'Payouts',
+                    path:'transactions/policy-service/payouts'
+                },
+                {
+                    id:'non-payouts',
+                    label:'Non Payouts',
+                    children:[{
+                        id:'email-update',
+                        label:'Email Update',
+                        path:'transactions/policy-service/non-payouts/email-update'
+                    },
+                    {
+                        id:'mobile-update',
+                        label:'Mobile Update',
+                        path:'transactions/policy-service/non-payouts/mobile-update'
+                    }
+                    ]
+
+                }
+            ]
+            }
         ]
     },
     {
-        id: 'Reports',
+        id: 'reports',
         label: 'Reports',
         icon: Summary,
         children: [
+            {
+                id:'mis-report',
+                label:'MIS Report',
+                path:'reports/mis-report'
+
+            }
         ]
     },
     {
-        id: 'Profile',
+        id: 'profile',
         label: 'Profile',
         icon: UserPen,
         children: [
         ]
     },
     {
-        id: 'ClosedPAN',
+        id: 'closed-pan',
         label: 'Closed PAN',
         icon: MessageCircleX,
         children: [
