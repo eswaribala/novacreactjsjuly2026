a8bae8cd81010e6d362e8369cff7194980edfa74
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-06 11:59:10 +0530

    updated

--- kyc-app/src/components/organisms/AddPolicy/AddPolicy.jsx
+++ kyc-app/src/components/organisms/AddPolicy/AddPolicy.jsx
@@ -1,9 +1,118 @@
+import { useState } from "react";
+import FormField from "../../molecules/FormField/FormField";
+import Message from "../../atoms/Message/Message";
+import Button from "../../atoms/Button/Button";
+const initialValues={
+   policyNo:0,
+   policyHolderName:"",
+   beneficiaryType:"",
+   documentType:"",
+   documentNumber:"",
+}
+
 function AddPolicy() {
+
+  const[values,setValues]=useState(initialValues);
+  const[errorMessages,setErrorMessages]=useState({});
+
+  const handleChange=(e)=>{
+    const {id,value}=e.target;
+    setValues({...values,[id]:value});
+  }
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    
+  }
+
   return (
-    <div>
-      <h2>Add Policy</h2>
-      {/* Add your form or content for adding a policy here */}
+    <>
+    <form onSubmit={handleSubmit} noValidate
+  className="space-y-4">
+         {errorMessages.general && (
+        <Message
+          type="error"
+          text={errorMessages.general}
+          className="mb-4"
+        />
+      )}  
+      <FormField 
+       id="policyNo" 
+       label="Policy Number" 
+       type="text" 
+       value={values.policyNo} 
+       onChange={handleChange} 
+       required 
+       placeholder="Enter your policy number" 
+       error={errorMessages.policyNo}
+       className="mb-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+
+       <FormField 
+       id="policyHolderName" 
+       label="Policy Holder Name" 
+       type="text" 
+       value={values.policyHolderName} 
+       onChange={handleChange} 
+       required 
+       placeholder="Enter the policy holder name" 
+       error={errorMessages.policyHolderName}
+       className="mb-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+       <FormField 
+       id="beneficiaryType" 
+       label="Beneficiary Type"
+       type="text" 
+       value={values.beneficiaryType} 
+       onChange={handleChange} 
+       required 
+       placeholder="Enter the beneficiary type" 
+       error={errorMessages.beneficiaryType}
+       className="mb-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+       <FormField 
+       id="documentType" 
+       label="Document Type" 
+       type="text" 
+       value={values.documentType} 
+       onChange={handleChange}        
+       required 
+       error={errorMessages.documentType}
+       placeholder="Enter the document type" 
+       className="mb-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+
+      <FormField 
+       id="documentNumber" 
+       label="Document Number" 
+       type="text" 
+       value={values.documentNumber} 
+       onChange={handleChange}        
+       required 
+       error={errorMessages.documentNumber}
+       placeholder="Enter the document number" 
+       className="mb-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" /> 
+    <div className="flex justify-center">
+    <Button type="submit" disabled={false}
+    className="h-14
+      
+      w-72
+      rounded-xl
+      bg-blue-600
+      text-lg
+      font-semibold
+      text-white
+      shadow-md
+      transition-all
+      duration-300
+      hover:bg-blue-700
+      hover:shadow-lg
+      focus:outline-none
+      focus:ring-4
+      focus:ring-blue-200">
+      {'Add Policy'}
+    </Button>
     </div>
+    
+    
+    </form>
+    </>
   );
 }
 export default AddPolicy;
