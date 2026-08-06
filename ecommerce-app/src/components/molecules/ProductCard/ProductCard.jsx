11d63417285d40d56c490e2b7a8c7460d86a40de
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-06 17:24:01 +0530

    updated

--- ecommerce-app/src/components/molecules/ProductCard/ProductCard.jsx
+++ ecommerce-app/src/components/molecules/ProductCard/ProductCard.jsx
@@ -1,4 +1,5 @@
 //create product card component using tailwind css
+import defaultImage from "../../../assets/defaultimage.png";
 const cardColors=["bg-red-100",
     "bg-green-100","bg-blue-100","bg-yellow-100",
     "bg-purple-100","bg-pink-100","bg-indigo-100","bg-gray-100"];
@@ -9,6 +10,7 @@ function ProductCard({ product,index }) {
     <div className={`overflow-hidden border border-gray-200  
      ${randomColor} shadow-md rounded-lg p-4 transition-transform 
      transform hover:scale-105 hover:shadow-lg`}>
+      <img src={product.image || defaultImage} alt={product.name} className="w-full h-48 object-cover mb-4 rounded" />
       <h3 className="text-lg font-semibold mb-2">{product.name}</h3>
       <p className="text-gray-600 mb-2">{product.description}</p>
       <p className="text-gray-800 font-bold">{new 
