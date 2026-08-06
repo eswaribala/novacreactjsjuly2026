b8b6b009f1da564606f0d13ce4554b089a1f1b49
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-06 12:06:35 +0530

    updated

---
+++ kyc-app/src/data/documentTypeData.js
@@ -0,0 +1,28 @@
+export const documentTypeData = [
+    {
+        id: 1,
+        name: 'Adhar Card',
+        description: 'An identity adhar card is an official document for verification'
+    },
+    {
+        id: 2,
+        name: 'Passport',
+        description: 'An identity passport is an official document for verification'
+    },
+    {
+        id: 3,
+        name: 'Voter ID',
+        description: 'An identity voter ID is an official document for verification'
+
+    },
+    {
+        id: 4,
+        name: 'Driving License',
+        description: 'An identity driving license is an official document for verification'
+    },
+    {
+        id: 5,
+        name: 'PAN Card',
+        description: 'An identity PAN card is an official document for verification'
+    }
+];
