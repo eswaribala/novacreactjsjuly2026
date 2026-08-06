f2a2c0b7b23946f2cca526729fa0d2d276faf5fb
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-06 12:15:08 +0530

    updated

---
+++ kyc-app/src/components/atoms/DropDownList/DropDownList.jsx
@@ -0,0 +1,26 @@
+function DropDownList({ id, name,options,placeholder, required,className, onChange, onSelect, selectedOption }) {
+
+    return(
+        <div className="dropdown">
+            <select id={id} name={name} 
+            value={selectedOption} 
+            onChange={onChange}
+            onSelect={onSelect}
+            required={required}
+            className={className}
+            >
+                <option value="">{placeholder}</option>
+                {options.map((option) => (                   
+
+                    <option key={option.value} value={option.value}>
+                        {option.label}
+                    </option>
+                ))}
+            </select>
+        </div>
+
+    )
+
+}
+
+export default DropDownList;
