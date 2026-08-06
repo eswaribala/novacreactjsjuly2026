1ac6861fa5cfbd92989b8d033a07a240731b28d0
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-06 12:02:31 +0530

    updated

---
+++ kyc-app/src/data/beneficiaryTypeData.js
@@ -0,0 +1,23 @@
+export const beneficiaryTypeData = [
+  {
+    id: 1,
+    name: 'Individual',
+    description: 'An individual beneficiary is a single person who receives benefits or assistance from a program or organization. This type of beneficiary is typically eligible for support based on personal circumstances, such as income level, health status, or other qualifying factors.'
+  },
+  {
+    id: 2,
+    name: 'Life Assured',
+    description: 'A life assured beneficiary is an individual whose life is insured under a life insurance policy. This type of beneficiary is typically eligible to receive the policy benefits upon the occurrence of the insured event, such as the death of the insured person.'
+  },
+  {
+    id: 3,
+    name: 'Proposal',
+    description: 'A proposal beneficiary refers to an individual or entity named in an insurance proposal form to receive benefits or assistance from a program or organization. This type of beneficiary is typically eligible for support based on the terms outlined in the proposal.'
+  },
+   {
+    id: 4,
+    name: 'Legal Heir',
+    description: 'A legal heir beneficiary refers to an individual who is entitled to inherit the assets or benefits of a deceased person according to the law. This type of beneficiary is typically eligible for support based on legal inheritance rights.'
+  }
+
+];
