d0008dfcfc5ab16b7c8857763acb9634f35b599a
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-07 10:16:26 +0530

    updated

---
+++ kyc-auth-api/controllers/policyController.js
@@ -0,0 +1,30 @@
+const Policy=require('../models/policyModel');
+
+const savePolicy = async (req, res) => {
+
+    console.log('Received policy request:', req.body);
+    try {
+        const { policyNo, policyHolderName, beneficiaryType, documentType, documentNumber } = req.body;
+
+        if (!policyNo || !policyHolderName || !beneficiaryType || !documentType || !documentNumber) {
+            return res.status(400).json(
+                { message: 'All fields are required' });
+        }
+
+        const existingPolicy = await Policy.findOne({ policyNo });
+        if (existingPolicy) {
+            return res.status(400).json(
+                { message: 'Policy already exists' });
+        }
+
+        const newPolicy = new Policy({ policyNo, policyHolderName, beneficiaryType, documentType, documentNumber });
+        await newPolicy.save();
+        res.status(201).json(
+            { message: 'Policy saved successfully' });
+    } catch (error) {
+        res.status(500).json(
+            { message: 'Server error' });
+    }
+};
+
+module.exports = { registerUser };
