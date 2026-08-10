c9975ac8130c190bc9ce3625595313163c8c0370
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-10 12:21:34 +0530

    updated

--- kyc-app/src/pages/BeneficiaryPAN.jsx
+++ kyc-app/src/pages/BeneficiaryPAN.jsx
@@ -1,9 +1,59 @@
+import {useDispatch, useSelector} from 'react-redux';
+import {useEffect} from 'react';
+import { getPolicyByCustomerName } from '../redux/features/policy/policySlicer';
+import DropDownList from '../components/atoms/DropDownList/DropDownList';
+
+import {useAuth} from '../hooks/useAuth';
+
 function BeneficiaryPAN() {
+
+  const dispatch = useDispatch();
+  const { user} = useAuth();
+
+  const { policies, status, loading, error } = useSelector((state) => state.policy);
+
+
+  useEffect(() => {
+    // You can dispatch actions or perform side effects here if needed
+    dispatch(getPolicyByCustomerName(user));
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+ 
+  if (status === 'success' && policies.length === 0) {
+    return <div>No policies found for the user.</div>;
+  }
+
+  const handleChange = (e) => {
+    console.log('Selected policy:', e.target.value);
+    // Handle the change event as needed
+  }
+
   return (
-    <div>
-      <h1>Beneficiary PAN</h1>
-      {/* Add your Beneficiary PAN form or functionality here */}
-    </div>
+    
+    <>
+     <DropDownList
+        id="policyNo"
+        name="policyNo"
+        options={policies.map((policy) => ({
+          value: policy.policyNo,
+          label: policy.policyNo,
+        }))}
+        placeholder="Select the document type"
+        required
+        className="mb-6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+        onChange={handleChange}
+        selectedOption={policies.length > 0 ? policies[0].policyNo : ''}
+       />
+
+    </>
+
   );
 }
 
