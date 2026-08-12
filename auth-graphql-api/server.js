ece99ce6d516ed9fa80915bb59e21fcefd560adc
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-12 14:57:17 +0530

    updated

--- auth-graphql-api/server.js
+++ auth-graphql-api/server.js
@@ -1,55 +1,33 @@
 require("dotenv").config();
-
-const express = require("express");
-const cors = require("cors");
-
-
-const { connectToDatabase } = require("./config/database");
-
-
-const app = express();
-
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL
-    //credentials: true,
-  })
-);
-
-app.use(express.json());
-
-app.get("/api/health", (req, res) => {
-  res.status(200).json({
-    status: "ok",
-  });
-});
-
-
-
-
-// Global error handler
-app.use((err, req, res, next) => {
-  console.error(err);
-
-  res.status(err.status || 500).json({
-    message: err.message || "Unexpected server error",
-  });
-});
-
-const port = process.env.PORT || 5000;
+const { ApolloServer } = require("@apollo/server");
+const {startStandaloneServer} = require("@apollo/server/standalone");
+const {connectToDatabase} = require("./config/database.js");
+const typeDefs = require("./graphql/typeDefs.js");
+const resolvers = require("./graphql/resolvers.js");
 
 async function startServer() {
   try {
+    // Connect to the database
     await connectToDatabase();
-
-    app.listen(port, () => {
-      console.log(`API running on http://localhost:${port}`);
-      console.log(`Swagger UI: http://localhost:${port}/api-docs`);
+    const server = new ApolloServer({
+      typeDefs,
+      resolvers,
     });
-  } catch (error) {
-    console.error("Failed to start server:", error);
-    process.exit(1);
+    const { url } = await startStandaloneServer(
+    server,
+    {
+      listen: {
+        port: Number(process.env.PORT) || 4000
+      }
+    }
+  );
+  console.log(`Server running at: ${url}`);
+}
+
+  catch (error) {
+    console.error("Error starting the server:", error);
   }
+
 }
 
 startServer();
