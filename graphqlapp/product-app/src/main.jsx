04e9d7f72858b0aac4da714e8ee585a6b0d4581d
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-12 16:03:32 +0530

    updated

--- graphqlapp/product-app/src/main.jsx
+++ graphqlapp/product-app/src/main.jsx
@@ -7,7 +7,10 @@ import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 import { ApolloProvider } from '@apollo/client/react'
 
 const client=new ApolloClient({
-  uri: new HttpLink({uri:'http://localhost:4000/graphql'}),
+  link:new HttpLink({
+      uri:'http://localhost:4000/graphql'
+  }),
+  
   cache:new InMemoryCache()
 })
 
