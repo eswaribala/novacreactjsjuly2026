f7cf5f8070e791c2e1e81f7c37ab6d5137c3ac57
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-13 11:16:11 +0530

    updated

--- loan-graphql-api/server.js
+++ loan-graphql-api/server.js
@@ -1,47 +1,20 @@
 // deploy api
-express=require('express');
-cors=require('cors');
-dotenv=require('dotenv');
-dotenv.config();
-connectDB=require('./config/database');
-swaggerSpecification=require('./config/swagger');
-swaggerUi=require('swagger-ui-express');
-
-
-app=express();
-connectDB();
-
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL
-    //credentials: true,
-  })
-);
-
-//define endpoints
-app.use(express.json());
-//routes
-app.use('/api-docs', swaggerUi.serve, 
-    swaggerUi.setup(swaggerSpecification));
-//Optional OpenAPI JSON endpoint
-app.get("/api-docs.json", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  res.send(swaggerSpecification);
-});
-
-
-app.use('/api/partner', require('./routes/partnerRoutes'));
-app.use('/api/health', async (req, res) => {
-    res.status(200).json({ message: 'API is healthy' });
-}
-);
-const PORT = process.env.PORT || 5000;
-
-function startServer() {
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-        console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
+require('dotenv').config();
+const connectDB = require('./config/database');
+const typeDefs = require('./graphql/typedef.js');
+const resolvers = require('./graphql/resolver.js');
+const { ApolloServer } = require('apollo-server');
+async function startServer() {
+   await connectDB();
+    const server = new ApolloServer({
+        typeDefs: typeDefs,
+        resolvers: resolvers,
     });
+    // start as standalone server
+    const { url } = await server.listen({ port: 4000 });
+    console.log(`🚀 Server ready at ${url}`);
+
 }
+
 
 startServer();
