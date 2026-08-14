e16713da9c7f4377650b20c1b2030b2a5844427e
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-14 11:15:43 +0530

    updated

--- novaceshop/src/app/index.tsx
+++ novaceshop/src/app/index.tsx
@@ -1,11 +1,13 @@
 import * as Device from "expo-device";
-import { Platform, StyleSheet } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
-import { BottomTabInset, MaxContentWidth, Spacing } from "@/constants/theme";
 
+import { BottomTabInset, MaxContentWidth, Spacing } from "@/constants/theme";
+import { ScrollView } from "react-native";
+import { products } from "../data/products";
 function getDevMenuHint() {
   if (Platform.OS === "web") {
     return <ThemedText type="small">use browser devtools</ThemedText>;
@@ -25,11 +27,40 @@ function getDevMenuHint() {
   );
 }
 
+//show products in a list with image title and price and description and add to cart button with react native and expo router
 export default function HomeScreen() {
   return (
     <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}></SafeAreaView>
+      <SafeAreaView style={styles.safeArea}>
+        <ScrollView
+          style={{ flex: 1, width: "100%" }}
+          contentContainerStyle={{ flexGrow: 1 }}
+        >
+          <ThemedText style={styles.title}>
+            Welcome to Novac Shopify!
+          </ThemedText>
+          {products.map((product) => (
+            <View key={product.id} style={styles.productContainer}>
+              <Card product={product}></Card>
+            </View>
+          ))}
+        </ScrollView>
+      </SafeAreaView>
     </ThemedView>
+  );
+}
+
+function Card({ product }: { product: (typeof products)[number] }) {
+  return (
+    <View style={styles.card}>
+      <ThemedText>{product.id}</ThemedText>
+      <ThemedText>{product.name}</ThemedText>
+      <ThemedText>{product.description}</ThemedText>
+      <ThemedText>Price: ${product.price}</ThemedText>
+      <View style={styles.button}>
+        <ThemedText>Add to Cart</ThemedText>
+      </View>
+    </View>
   );
 }
 
@@ -66,5 +97,29 @@ const styles = StyleSheet.create({
     paddingHorizontal: Spacing.three,
     paddingVertical: Spacing.four,
     borderRadius: Spacing.four,
+  },
+  productContainer: {
+    marginBottom: Spacing.four,
+  },
+  card: {
+    padding: Spacing.four,
+    borderRadius: Spacing.four,
+    backgroundColor: "#fff",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  button: {
+    marginTop: Spacing.two,
+    paddingVertical: Spacing.two,
+    paddingHorizontal: Spacing.three,
+    borderRadius: Spacing.three,
+    backgroundColor: "#007bff",
+    alignItems: "center",
   },
 });
