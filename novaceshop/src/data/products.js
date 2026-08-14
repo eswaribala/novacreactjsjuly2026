fb0c38db8db70d72fa13d7bb36ed86ce566a2a10
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-14 11:05:34 +0530

    updated

---
+++ novaceshop/src/data/products.js
@@ -0,0 +1,37 @@
+export const products = [
+  {
+    id: 1,
+    name: 'TV',
+    price: 500000,
+    image: 'https://unsplash.com/photos/TV.jpg',
+    description: 'A high-definition television with smart features.',
+  },
+  {
+    id: 2,
+    name: 'Laptop',
+    price: 1500000,
+    image: 'https://unsplash.com/photos/Laptop.jpg',
+    description: 'A powerful laptop for work and gaming.',
+  },
+  {
+    id: 3,
+    name: 'Smartphone',
+    price: 800000,
+    image: 'https://unsplash.com/photos/Smartphone.jpg',
+    description: 'A smartphone with the latest features.',
+  },
+  {
+    id: 4,
+    name: 'Headphones',
+    price: 200000,
+    image: 'https://unsplash.com/photos/Headphones.jpg',
+    description: 'Noise-cancelling over-ear headphones.',
+  },
+  {
+    id: 5,
+    name: 'Smartwatch',
+    price: 300000,
+    image: 'https://unsplash.com/photos/Smartwatch.jpg',
+    description: 'A smartwatch with fitness tracking features.',
+  },
+];
