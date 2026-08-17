18c0e2a0320f07aaaf2c88109ca9891f41ec5c80
Parameswari Ettiappan <parameswaribala@gmail.com>
2026-08-17 11:32:21 +0530

    updated

---
+++ kyc-app/src/components/atoms/Input/Input.test.jsx
@@ -0,0 +1,44 @@
+import {describe, it, expect,vi} from 'vitest';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Input from './Input';
+describe('Input component', () => {
+
+    it('should render the input component', () => {
+        render(<Input />);
+        const inputElement = screen.getByRole('textbox');
+        expect(inputElement).toBeInTheDocument();
+    });
+
+    it('should render the input component with a placeholder', () => {
+        const placeholderText = 'Enter your name';
+        render(<Input placeholder={placeholderText} />);
+        const inputElement = screen.getByPlaceholderText(placeholderText);
+        expect(inputElement).toBeInTheDocument();
+    });
+
+    it('should render the input component with a value', () => {
+        const valueText = 'John Doe';
+        render(<Input value={valueText} />);
+        const inputElement = screen.getByDisplayValue(valueText);
+        expect(inputElement).toBeInTheDocument();
+    });
+    it("should call onChange when input value changes", () => {
+    const handleChange = vi.fn();
+
+    render(
+      <Input
+        placeholder="Enter text"
+        onChange={handleChange}
+      />
+    );
+
+    const inputElement =
+      screen.getByPlaceholderText("Enter text");
+
+    fireEvent.change(inputElement, {
+      target: { value: "New Value" },
+    });
+
+    expect(handleChange).toHaveBeenCalled();
+  });
+});
